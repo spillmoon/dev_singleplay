811a883f1f65ecb4621043b84603b7eaf61defb5
spillmoon <spillmoon@naver.com>
2016-08-26 17:49:55 +0900

    공연목록(뮤지컬-정렬3가지) 구현

--- models/playlist.js
+++ models/playlist.js
@@ -2,8 +2,40 @@ var mysql = require('mysql');
 var dbPool = require('../models/common').dbPool;
 
 // todo: 뮤지컬 목록(정렬 방식에 따른 목록 정렬)
-function musicalList() {
+function musicalList(sort, callback) {
+    var sql_star = "select py.id pid, name, '뮤지컬', placeName, playDay, playTime, VIPprice, starScoreAvg " +
+                "from play py join place pe on (py.place_id = pe.id) " +
+                "where playDay = '2016-09-01' and theme = 0 " +
+                "order by starScoreAvg desc";
+    var sql_time = "select py.id pid, name, '뮤지컬', placeName, playDay, playTime, VIPprice, starScoreAvg " +
+                    "from play py join place pe on (py.place_id = pe.id) " +
+                    "where playDay = '2016-09-01' " +
+                    "order by playTime asc";
+    var sql_sale = "select py.id pid, name, '뮤지컬', placeName, playDay, playTime, VIPprice, starScoreAvg " +
+                    "from play py join place pe on (py.place_id = pe.id) " +
+                    "where playDay = '2016-09-01' " +
+                    "order by salePer desc";
+    var sql = "";
+    if (sort == 0)
+        sql = sql_star;
+    if (sort == 1)
+        sql = sql_time;
+    if (sort == 2)
+        sql = sql_sale;
 
+    dbPool.getConnection(function(err, dbConn) {
+        if (err) {
+            return callback(err);
+        }
+        dbConn.query(sql, function(err, playlist) {
+            dbConn.release();
+            if (err) {
+                return callback(err);
+            }
+            callback(null, playlist);
+        })
+
+    });
 }
 // todo: 오페라 목록(정렬 방식에 따른 목록 정렬)
 function operaList() {
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Play = require('../models/playlist');
 
 // GET, 항목별 정렬된 공연 목록
 router.get('/', function (req, res, next) {
     var startIndex = parseInt(req.query.start, 10);
+    var action = req.query.action || 0;
 
-    if (req.query.action == 0) {
-        res.send({
-            totalItems: 140,
-            itemsPerPage: 10,
-            startIndex: startIndex,
-            paging: {
-                prev: "http://server:port/playlists/?action=0&theme=0&sort=0&start=" + (startIndex-10),
-                next: "http://server:port/playlists/?action=0&theme=0&sort=0&start=" + (startIndex+10)
-            },
-            results: [
-                {
-                    playId: 1,
-                    playName: "위키드",
-                    theme: "뮤지컬",
-                    placeName: "디큐브 아트센터",
-                    playDay: "2016-08-22",
-                    playTime: "19:00",
-                    price: 80000,
-                    salePrice: 68000,
-                    starScore: 5,
-                    poster: "http://server:port/images/poster/filename.jpg"
-                }, {}, {}
-            ]
-        });
+    if (action == 0) { // 항목별 검색
+        var theme = req.query.theme || 0;
+        var sort = req.query.sort || 0;
+        if (theme == 0) { // 뮤지컬 목록
+            Play.musicalList(sort, function(err, playlist) {
+                if (err) {
+                    return next(err);
+                }
+                res.send({
+                    totalItems: 140,
+                    itemsPerPage: 10,
+                    startIndex: startIndex,
+                    paging: {
+                        prev: "http://server:port/playlists/?action=0&theme=0&sort=0&start=" + (startIndex-10),
+                        next: "http://server:port/playlists/?action=0&theme=0&sort=0&start=" + (startIndex+10)
+                    },
+                    results: playlist
+                });
+            });
+        } else if (theme == 1) { // 오페라 목록
+            Play.operaList();
+        } else { // 콘서트 목록
+            Play.concertList();
+        }
     }
     else if (req.query.action == 1) {
         res.send({
