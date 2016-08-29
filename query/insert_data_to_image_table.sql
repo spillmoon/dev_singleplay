94fb27cf44d56f9940dd27a64021743600d3423f
spillmoon <spillmoon@naver.com>
2016-08-29 20:36:32 +0900

    공연, 이미지 데이터 추가

--- query/insert_data_to_image_table.sql
+++ query/insert_data_to_image_table.sql
@@ -1,5 +1,6 @@
 insert into image (play_name, imageName, imagePath, imageType)
 values ('잭더리퍼', 'play1_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play1_poster.jpg', 0);
+
 insert into image (play_name, imageName, imagePath, imageType)
 values ('잭더리퍼', 'play1_cast1.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play1_cast1.png', 1);
 insert into image (play_name, imageName, imagePath, imageType)
@@ -29,6 +30,9 @@ values ('잭더리퍼', 'play1_cast13.png', 'c:\\didimdol\\nodejs\\dev_singlepla
 
 
 insert into image (play_name, imageName, imagePath, imageType)
+values ('키다리아저씨', 'play2_poster.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play2_poster.png', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
 values ('키다리아저씨', 'play2_cast1.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play2_cast1.png', 1);
 insert into image (play_name, imageName, imagePath, imageType)
 values ('키다리아저씨', 'play2_cast2.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play2_cast2.png', 1);
@@ -38,3 +42,186 @@ insert into image (play_name, imageName, imagePath, imageType)
 values ('키다리아저씨', 'play2_cast4.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play2_cast4.png', 1);
 insert into image (play_name, imageName, imagePath, imageType)
 values ('키다리아저씨', 'play2_cast5.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play2_cast5.png', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play3_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast1.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast1.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast2.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast2.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast3.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast3.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast4.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast4.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast5.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast5.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast6.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast6.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast7.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast7.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast8.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast8.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast9.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast9.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast10.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast10.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast11.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast11.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast12.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast12.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast13.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast13.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast14.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast14.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast15.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast15.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast16.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast16.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast17.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast17.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast18.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast18.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast19.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast19.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast20.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast20.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('그날들', 'play3_cast21.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play3_cast21.png', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play4_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast1.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast1.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast2.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast2.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast3.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast3.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast4.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast4.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast5.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast5.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast6.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast6.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast7.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast7.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast8.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast8.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast9.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast9.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast10.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast10.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast11.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast11.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast12.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast12.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast13.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast13.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast14.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast14.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast15.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast15.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast16.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast16.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('페스트', 'play4_cast17.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play4_cast17.png', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 백지영 전국투어 콘서트 ‘ANDANTE’- 대전', 'play5_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play5_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 백지영 전국투어 콘서트 ‘ANDANTE’- 대전', 'play5_cast.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play5_cast.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play6_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_cast1.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play6_cast1.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_cast2.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play6_cast2.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_cast3.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play6_cast3.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_cast4.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play6_cast4.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 심플 플랜 내한공연', 'play6_cast5.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play6_cast5.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 이선희 The Great Concert', 'play7_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play7_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 이선희 The Great Concert', 'play7_cast.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play7_cast.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016클래지콰이 콘서트', 'play8_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play8_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016클래지콰이 콘서트', 'play8_cast1.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play8_cast1.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016클래지콰이 콘서트', 'play8_cast2.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play8_cast2.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016클래지콰이 콘서트', 'play8_cast3.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play8_cast3.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016클래지콰이 콘서트', 'play8_cast4.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play8_cast4.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play9_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast1.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast1.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast2.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast2.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast3.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast3.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast4.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast4.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast5.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast5.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast6.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast6.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast7.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast7.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast8.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast8.bmp', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('피아X드럭 레스토랑 <REAL LIVE>', 'play9_cast9.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play9_cast9.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 이선희 The Great Concert - 대구', 'play7_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play7_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('2016 이선희 The Great Concert - 대구', 'play7_cast.bmp', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play7_cast.bmp', 1);
+
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_poster.jpg', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\poster\\play10_poster.jpg', 0);
+
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast1.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast1.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast2.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast2.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast3.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast3.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast4.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast4.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast5.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast5.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast6.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast6.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast7.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast7.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast8.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast8.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast9.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast9.png', 1);
+insert into image (play_name, imageName, imagePath, imageType)
+values ('스위니토드', 'play10_cast10.png', 'c:\\didimdol\\nodejs\\dev_singleplay\\uploads\\images\\cast\\play10_cast10.png', 1);
--- query/insert_data_to_play_table.sql
+++ query/insert_data_to_play_table.sql
@@ -233,3 +233,93 @@ VALUES ('페스트', 4, '2016-09-25', date_format('2016-09-25 14:00', '%H:%i'), 
 INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
 VALUES ('페스트', 4, '2016-09-25', date_format('2016-09-25 18:30', '%H:%i'), 0, 140000, 120000, 90000, 50, 9.3);
 
+
+
+
+
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 심플 플랜 내한공연', 9, str_to_date('2016-09-01', '%Y-%m-%d'), date_format('2016-09-01 20:00', '%H:%i'), 2, 1110000, 1110000, 1110000, 60, 8.1);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 이선희 The Great Concert', 5, str_to_date('2016-09-02', '%Y-%m-%d'), date_format('2016-09-02 20:00', '%H:%i'), 2, 154000, 132000, 99000, 50, 9.9);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 이선희 The Great Concert', 5, str_to_date('2016-09-03', '%Y-%m-%d'), date_format('2016-09-03 18:00', '%H:%i'), 2, 154000, 132000, 99000, 50, 9.9);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 이선희 The Great Concert', 5, str_to_date('2016-09-04', '%Y-%m-%d'), date_format('2016-09-04 17:00', '%H:%i'), 2, 154000, 132000, 99000, 50, 9.9);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('피아X드럭 레스토랑 <REAL LIVE>', 9, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 18:00', '%H:%i'), 2, 66000, 66000, 66000, 50, 7.3);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 백지영 전국투어 콘서트 ‘ANDANTE’- 대전', 6, str_to_date('2016-09-10', '%Y-%m-%d'), date_format('2016-09-10 19:00', '%H:%i'), 2, 132000, 110000, 99000, 45, 9.6);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 백지영 전국투어 콘서트 ‘ANDANTE’- 인천', 7, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 17:00', '%H:%i'), 2, 132000, 110000, 99000, 45, 9.6);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 이선희 The Great Concert - 대구', 8, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 19:00', '%H:%i'), 2, 143000, 132000, 110000, 30, 9.9);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016클래지콰이 콘서트', 9, str_to_date('2016-09-24', '%Y-%m-%d'), date_format('2016-09-24 19:00', '%H:%i'), 2, 90000, 90000, 90000, 40, 8.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('2016 이선희 The Great Concert - 대구', 8, str_to_date('2016-09-25', '%Y-%m-%d'), date_format('2016-09-25 17:00', '%H:%i'), 2, 143000, 132000, 110000, 30, 9.9);
+
+
+
+
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-01', date_format('2016-09-01 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-02', date_format('2016-09-02 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-03', date_format('2016-09-03 15:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-03', date_format('2016-09-03 19:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-04', date_format('2016-09-04 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-04', date_format('2016-09-04 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-06', date_format('2016-09-06 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-07', date_format('2016-09-07 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-08', date_format('2016-09-08 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-09', date_format('2016-09-09 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-10', date_format('2016-09-10 15:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-10', date_format('2016-09-10 19:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-11', date_format('2016-09-11 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-11', date_format('2016-09-11 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-13', date_format('2016-09-13 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-14', date_format('2016-09-14 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-14', date_format('2016-09-14 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-16', date_format('2016-09-16 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-16', date_format('2016-09-16 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-17', date_format('2016-09-17 15:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-17', date_format('2016-09-17 19:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-18', date_format('2016-09-18 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-18', date_format('2016-09-18 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-20', date_format('2016-09-20 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-21', date_format('2016-09-21 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-22', date_format('2016-09-22 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-23', date_format('2016-09-23 20:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-24', date_format('2016-09-24 15:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-24', date_format('2016-09-24 19:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-25', date_format('2016-09-25 14:00', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
+INSERT INTO play (name, place_id, playDay, playTime, theme, VIPprice, Rprice, Sprice, salePer, starScoreAvg)
+VALUES ('스위니토드', 10, '2016-09-25', date_format('2016-09-25 18:30', '%H:%i'), 0, 140000, 120000, 80000, 60, 9.7);
