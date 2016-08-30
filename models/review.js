8dabc23545e97b355b2e13132ee00f9a6cc65b91
han <koocoo6@naver.com>
2016-08-30 10:45:01 +0900

    리뷰 추가

--- models/review.js
+++ models/review.js
@@ -1,3 +1,21 @@
-/**
- * Created by Tacademy on 2016-08-23.
- */
+var mysql = require('mysql');
+var dbPool = require('../models/common').dbPool;
+
+function createReview(userId, playId, playName, starScore, callback) {
+    var sql = 'insert into starScore(user_id, play_id, play_name, starScore) ' +
+                "values (?, ?, ?, ?)";
+
+    dbPool.getConnection(function(err, dbConn) {
+        if (err) {
+            return callback(err);
+        }
+        dbConn.query(sql, [userId, playId, playName, starScore], function(err) {
+           if (err) {
+               return callback(err);
+           }
+           callback(null);
+        });
+    });
+}
+
+module.exports.createReview = createReview;
--- routes/review.js
+++ routes/review.js
@@ -2,18 +2,26 @@ var express = require('express');
 var router = express.Router();
 var isSecure = require('./common').isSecure;
 var isAuthenticated = require('./common').isAuthenticated;
+var Review = require('../models/review')
 
 // POST, 별점 주기
-router.post('/', isSecure, isAuthenticated, function(req, res, next) {
-    var playId = req.body.pid;
+router.post('/', isSecure, isAuthenticated, function (req, res, next) {
+    var userId = req.body.userId;
+    var playId = req.body.playId;
+    var playName = req.body.playName;
     var starScore = req.body.starScore;
 
-    res.send({
-        result:
-        {
-            message: "평가해주셔서 감사합니다."
+    Review.createReview(userId, playId, playName, starScore, function (err) {
+        if (err) {
+            return next(err);
         }
+        res.send({
+            result: {
+                message: "평가해주셔서 감사합니다."
+            }
+        });
     });
+
 });
 
 module.exports = router;
