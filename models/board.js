50cf38d0cb0bbabf3b3172c57ffb33212db3e8c1
han <koocoo6@naver.com>
2016-08-31 15:34:46 +0900

    board 쿼리 수정

--- models/board.js
+++ models/board.js
@@ -5,7 +5,7 @@ var url = require('url');
 var fs = require('fs');
 
 function listBoards(callback) {
-    var sql = 'SELECT boardNo, title, writeDate, titleFilePath FROM board';
+    var sql = 'SELECT boardNo, titleFileName FROM board;';
 
     dbPool.getConnection(function (err, dbConn) {
         if (err) {
@@ -20,8 +20,8 @@ function listBoards(callback) {
             for (var i=0; i<results.length; i++) {
                 board.push({
                     boardNo: results[i].boardNo,
-                    image : url.resolve('http://ec2-52-78-118-8.ap-northeast-2.compute.amazonaws.com:8080/boardimg/', path.basename(results[i].titleFilePath))
-                    // image : url.resolve('http://127.0.0.1:8080/boardimg/', path.basename(results[i].titleFilePath))
+                    image : url.resolve('http://ec2-52-78-118-8.ap-northeast-2.compute.amazonaws.com:8080/boardimg/', path.basename(results[i].titleFileName))
+                    //image : url.resolve('http://127.0.0.1:8080/boardimg/', path.basename(results[i].titleFileName))
                 });
             }
             callback(null, board);
@@ -30,7 +30,7 @@ function listBoards(callback) {
 }
 
 function findBoard(boardNo, callback) {
-    var sql = 'select boardNo, filePath from board where boardNo = ?';
+    var sql = 'select boardNo, fileName from board where boardNo = ?';
 
     dbPool.getConnection(function (err, dbConn) {
         if (err) {
@@ -41,7 +41,7 @@ function findBoard(boardNo, callback) {
             if (err) {
                 return callback(err);
             }
-            var board = url.resolve('http://ec2-52-78-118-8.ap-northeast-2.compute.amazonaws.com:8080/boardimg/', path.basename(result[0].filePath));
+            var board = url.resolve('http://ec2-52-78-118-8.ap-northeast-2.compute.amazonaws.com:8080/boardimg/', path.basename(result[0].fileName));
             // var board = url.resolve('http://127.0.0.1:8080/boardimg/', path.basename(result[0].filePath));
             callback(null, board);
         });
