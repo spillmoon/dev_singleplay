41f95f91dc9ad812cf5508badbfc97728930ed27
han <koocoo6@gmail.com>
2016-09-04 21:47:00 +0900

    좌석 선택 URL 쿼리문 추가

--- models/usableseat.js
+++ models/usableseat.js
@@ -2,6 +2,10 @@
 
 
 function seatInfo(playId, callback) {
+    // var sql = "select p.theme, p.name, p.playDay, p.playTime, pl.address, pl.placeName, p.placeImageName, (p.VIPprice*(100-p.saveOff)/100) VIPprice, (p.Rprice*(100-p.saveOff)/100) Rprice, (p.Sprice*(100-p.saveOff)/100) Sprice  " +
+    //"from play p join place pl on (pl.id = p.place_id) "  +
+   // "where p.playDay=curdate() and play_name='잭더리퍼'";
+
     var info = {};
 
     info.image = "http://ec2-52-78-118-8.ap-northeast-2.compute.amazonaws.com:80/placeimg/play1_201609012000.png";
