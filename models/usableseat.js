0429142bbe09b002f989840c30077bf446c656ac
han <koocoo6@naver.com>
2016-09-05 16:18:12 +0900

    오늘 날짜 빈좌석으로 수정

--- models/usableseat.js
+++ models/usableseat.js
@@ -5,10 +5,10 @@ var url = require('url');
 var fs = require('fs');
 
 function selectSeat(playId, callback) {
-    var sql = "select p.theme, p.name, p.playDay, p.playTime, pl.address, pl.placeName, p.placeImageName, u.seatClass, u.seatInfo, (p.VIPprice*(100-p.saveOff)/100) VIPprice, (p.Rprice*(100-p.saveOff)/100) Rprice, (p.Sprice*(100-p.saveOff)/100) Sprice " +
-        "from play p join place pl on (pl.id = p.place_id) " +
-        "join usableSeat u on (u.play_id = p.id) " +
-        "where p.playDay=str_to_date('2016-09-10', '%Y-%m-%d') and p.id=? and u.state=0";
+    var sql = "select p.theme, p.name, substring(p.playDay, 1, 10) playDay, substring(p.playTime, 1, 5) playTime, pl.address, pl.placeName, p.placeImageName, u.seatClass, u.seatInfo, (p.VIPprice*(100-p.saveOff)/100) VIPprice, (p.Rprice*(100-p.saveOff)/100) Rprice, (p.Sprice*(100-p.saveOff)/100) Sprice " +
+              "from play p join place pl on (pl.id = p.place_id) " +
+              "join usableSeat u on (u.play_id = p.id) " +
+              "where p.playDay=curdate() and p.id=? and u.state=0";
 
     dbPool.getConnection(function (err, dbConn) {
         if (err) {
